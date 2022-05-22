560466e209e2a32923245d72b232ba8f3464c7fb
Matan <matanbitton2@gmail.com>
2022-05-22 18:17:40 +0300

    make todo class and todolist class functionality

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import "./style.css";
+import ToDo from "./todo";
+import ToDoList from "./todolist";
+
+let myList = new ToDoList();
+let myTodo = new ToDo("title", "des", "date", "important");
+let moreTodo = new ToDo("movie", "black", "tommorow", "important");
+myList.addToDo(myTodo);
+myList.addToDo(moreTodo);
+console.log(myList);
+myList.removeToDo(myTodo);
+
+console.log(myList);
---
+++ src/todolist.js
@@ -0,0 +1,13 @@
+import ToDo from "./todo";
+
+export default class ToDoList {
+  constructor() {
+    this.toDos = [];
+  }
+  addToDo(toDo) {
+    this.toDos.push(toDo);
+  }
+  removeToDo(toDo) {
+    this.toDos = this.toDos.filter((item) => item["title"] == toDo["title"]);
+  }
+}
