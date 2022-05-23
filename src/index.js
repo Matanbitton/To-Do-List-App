56ac1fe216a700aea8c6fafa5ac9c611c73b01ef
Matan <matanbitton2@gmail.com>
2022-05-23 12:27:21 +0300

    add rendering todos functionality

--- src/index.js
+++ src/index.js
@@ -3,16 +3,19 @@ import ToDo from "./todo";
 import ToDoList from "./todolist";
 import Project from "./project";
 import ProjectsList from "./projectslist";
+import renderToDos from "./rendertodos";
 
+const toDoDisplayed = document.querySelector(".displayed-todos");
 let myList = new ToDoList();
 let myTodo = new ToDo("title", "des", "date", "important");
 let moreTodo = new ToDo("movie", "black", "tommorow", "important");
 myList.addToDo(myTodo);
 myList.addToDo(moreTodo);
+console.log(myList.toDos);
+toDoDisplayed.append(renderToDos(myList.toDos));
+
 let workProject = new Project("Art");
 workProject.addToProjectToDos(myList);
 let projectsList = new ProjectsList();
 
 projectsList.addToProjectList(workProject);
-
-console.log(projectsList);
--- src/rendertodos.js
+++ src/rendertodos.js
@@ -1,3 +1,9 @@
-import ToDoList from "./todolist";
-
-export default function renderToDos(toDos) {}
+export default function renderToDos(toDos) {
+  for (let i = 0; i < toDos.length - 1; i++) {
+    const toDoDiv = document.createElement("div");
+    toDoDiv.className = "todo";
+    toDoDiv.setAttribute = toDos[i].title;
+    toDoDiv.textContent = toDos[i].title;
+    return toDoDiv;
+  }
+}
