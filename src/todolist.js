85c72da26084cc0dacb8949e3eb229f8ab5d9658
Matan <matanbitton2@gmail.com>
2022-05-28 17:56:37 +0300

    fixed typo that caused some todos to not render

--- src/todolist.js
+++ src/todolist.js
@@ -1,11 +1,25 @@
+import ToDo from "./todo";
 export default class ToDoList {
   constructor() {
     this.toDos = [];
+    const toDo = new ToDo(
+      "Watch a movie with friends",
+      "",
+      "",
+      "low-importance"
+    );
+
+    this.toDos.push(toDo);
   }
   addToDo(toDo) {
     this.toDos.push(toDo);
   }
-  removeToDo(toDo) {
-    this.toDos = this.toDos.filter((item) => item["title"] == toDo["title"]);
+  removeToDo(toDoTitle) {
+    this.toDos = this.toDos.filter((item) => item["title"] !== toDoTitle);
+  }
+  checkIfEmpty() {
+    if (this.toDos.length == 0) {
+      return true;
+    }
   }
 }
