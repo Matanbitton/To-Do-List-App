95e825ba03200c0c35ce2ab3e8daaf411eb83624
Matan <matanbitton2@gmail.com>
2022-05-31 19:58:42 +0300

    make eL for Done and All buttons

--- src/index.js
+++ src/index.js
@@ -3,29 +3,96 @@ import ToDo from "./todo";
 import ToDoList from "./todolist";
 import Project from "./project";
 import ProjectsList from "./projectslist";
-import { renderToDos, listEmptyShowSVG } from "./rendertodos";
-import renderProjects from "./renderprojects";
+import jsConfetti from "canvas-confetti";
+
+import {
+  renderProjects,
+  renderToDos,
+  createToDoDivs,
+  listEmptyShowSVG,
+  formVisiblity,
+} from "./dom";
 import icon from "./noToDos.svg";
-import formVisiblity from "./toDoUserInput";
 
 (function ToDoListApp() {
-  formVisiblity();
-
+  const body = document.body;
   const toDoDisplayed = document.querySelector(".displayed-todos");
-  const form = document.querySelector(".add-todo-form");
-  const myList = new ToDoList();
-  renderToDos(myList.toDos);
+  const toDoList = new ToDoList();
+  const doneToDoButton = document.querySelector(".done-todos");
+  const allToDoButton = document.querySelector(".all-todos");
 
-  form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    const toDoTitle = document.getElementById("todo-title").value;
-    const toDoDescription = document.getElementById("todo-description").value;
-    const toDoDate = document.getElementById("todo-date").value;
-    const toDoPriority = document.getElementById("todo-priority").value;
-    const toDo = new ToDo(toDoTitle, toDoDescription, toDoDate, toDoPriority);
-    myList.addToDo(toDo);
-    renderToDos(myList.toDos);
+  let toDoDivList;
+
+  formVisiblity();
+  createToDoDivs(toDoList.toDos);
+  checkButtonEL();
+
+  body.addEventListener("click", (e) => {
+    // when user presses the form sumbit button this event listener fires and adds a new todo
+    if (e.target && e.target.id == "submit") {
+      e.preventDefault();
+
+      const toDoTitle = document.getElementById("todo-title").value;
+      const toDoDescription = document.getElementById("todo-description").value;
+      const toDoDate = document.getElementById("todo-date").value;
+      const toDoPriority = document.getElementById("todo-priority").value;
+      const toDo = new ToDo(toDoTitle, toDoDescription, toDoDate, toDoPriority);
+      toDoList.addToDo(toDo);
+      toDoDivList = createToDoDivs(toDoList.toDos);
+      checkButtonEL();
+    }
   });
+
+  doneToDoButton.addEventListener("click", () => {
+    let listDoneDivs = toDoList.toDos.filter((toDo) => toDo.done == true);
+
+    createToDoDivs(listDoneDivs);
+    checkButtonEL();
+  });
+
+  allToDoButton.addEventListener("click", () => {
+    let allToDo = toDoList.toDos;
+    console.log(allToDo);
+    toDoDisplayed.innerHTML = "";
+    createToDoDivs(allToDo);
+    checkButtonEL();
+  });
+  function checkButtonEL() {
+    const checkButton = document.querySelectorAll(".check");
+
+    checkButton.forEach((button) => {
+      button.addEventListener("click", (e) => {
+        if (
+          e.target.id == "check" &&
+          !e.target.parentNode.parentNode.classList.contains("todo-done")
+        ) {
+          e.target.parentNode.parentNode.classList.add("todo-done");
+          e.target.parentNode.parentNode.dataset.state = "done";
+          const targetToDoDiv = e.target.parentNode.parentNode.dataset.name;
+          let targetToDoObj = toDoList.getToDo(targetToDoDiv);
+          targetToDoObj.done = true;
+
+          let confetti = jsConfetti({
+            spread: 120,
+            particleCount: 200,
+            startVelocity: 100,
+            origin: { y: 1.1 },
+          });
+        } else {
+          e.target.parentNode.parentNode.classList.remove("todo-done");
+          e.target.parentNode.parentNode.dataset.state = "";
+
+          console.log(e.target.parentNode.parentNode);
+          const targetToDoDiv = e.target.parentNode.parentNode.dataset.name;
+          console.log(`targetToDoDiv = ${targetToDoDiv}`);
+          let targetToDoObj = toDoList.getToDo(targetToDoDiv);
+          targetToDoObj.done = false;
+          console.log(`targetToDoObj = ${targetToDoObj}`);
+          e.target.parentNode.parentNode.dataset.state = "";
+        }
+      });
+    });
+  }
 
   const myIcon = new Image();
   myIcon.className = "no-todos-img";
@@ -33,19 +100,18 @@ import formVisiblity from "./toDoUserInput";
 
   toDoDisplayed.addEventListener("click", (e) => {
     let toDoDiv = e.target.parentNode.parentNode;
-
     if (toDoDiv.dataset.name && e.target.className == "delete") {
-      myList.removeToDo(toDoDiv.dataset.name);
+      toDoList.removeToDo(toDoDiv.dataset.name);
       toDoDiv.remove();
-      console.log(myList.toDos);
-      if (myList.toDos.length == 0) {
+      console.log(toDoList.toDos);
+      if (toDoList.toDos.length == 0) {
         listEmptyShowSVG(toDoDisplayed, myIcon);
       }
     }
   });
 
   let workProject = new Project("Art");
-  workProject.addToProjectToDos(myList);
+  workProject.addToProjectToDos(toDoList);
   let projectsList = new ProjectsList();
 
   // this works and you need a function that deletes
