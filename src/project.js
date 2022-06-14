33c35c4bcefd1e5d3d211832fe828de4552a465f
Matan <matanbitton2@gmail.com>
2022-06-14 19:26:56 +0300

    rm unused variables and methods

--- src/project.js
+++ src/project.js
@@ -1,9 +1,5 @@
 export default class Project {
   constructor(projectName) {
     this.projectName = projectName;
-    this.projectToDos = [];
-  }
-  addToProjectToDos(toDoList) {
-    this.projectToDos.push(toDoList);
   }
 }
